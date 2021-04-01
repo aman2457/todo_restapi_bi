c42c63aec4bb2d91a12fe9e1bdeebfb1eb4e9453
aman2457 <amankumar84349@gmail.com>
2021-04-01 18:34:54 +0530

    PORT set for heroku

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv/config')
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = 3000;
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const TaskRouter = require('./routes/tasks')
@@ -26,6 +25,6 @@ mongoose.connect(
 
 
 
-app.listen(PORT, () => {
-    console.log('Server listening on ' + PORT);
+app.listen((process.env.PORT || 5000), () => {
+    console.log('Server listening on ');
 });
